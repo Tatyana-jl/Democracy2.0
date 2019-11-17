e079946f51b9c3318931bc5effffdde0c90d46fd
Jaroslav Sochan <sochan@emm.sk>
2019-11-17 10:24:23 +0100

    Microsoft azure api

--- Backend/recognize.py
+++ Backend/recognize.py
@@ -1,35 +1,76 @@
 import requests
-import matplotlib.pyplot as plt
 import json
-from PIL import Image
-from io import BytesIO
 import argparse
-import urllib
+import base64
+​
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-image', '--image', required=True)
     parser.add_argument('-sub_key', '--sub_key', required=False, default='c3aa71ab19bd40fcbe8459e7a18bc080')
-    # Add your Computer Vision subscription key and endpoint to your environment variables.
+​
     arguments = vars(parser.parse_args())
-    image_url = arguments['image']
+    image = arguments['image']
     subscription_key = arguments['sub_key']
+​
+    image = base64.b64decode(image)
     endpoint = 'https://northeurope.api.cognitive.microsoft.com/'
     analyze_url = endpoint + "vision/v2.1/analyze"
-    headers = {'Ocp-Apim-Subscription-Key': subscription_key}
-    params = {'visualFeatures': 'Categories,Description,Color'}
-    data = {'url': image_url}
-    response = requests.post(analyze_url, headers=headers, params=params, json=data)
+​
+    headers = {'Ocp-Apim-Subscription-Key': subscription_key,
+               'Content-Type': 'application/octet-stream'}
+    params = {'visualFeatures': 'Categories,Description,Adult'}
+    # data = {'url': image_url}
+    response = requests.post(analyze_url, headers=headers,
+                             params=params, data=image) #json=data)
     response.raise_for_status()
-    # The 'analysis' object contains various fields that describe the image. The most
-    # relevant caption for the image is obtained from the 'description' property.
+​
     analysis = response.json()
-    print(json.dumps(response.json()))
-    image_caption = analysis["description"]["captions"][0]["text"].capitalize()
-    # Display the image and overlay it with the caption.
-    # image = Image.open(BytesIO(requests.get(image_url).content))
-    # plt.imshow(image)
-    # plt.axis("off")
-    # _ = plt.title(image_caption, size="x-large", y=-0.1)
-    # plt.show()
-if __name__=='__main__':
+​
+    tags = analysis['description']['tags']
+    class_dict = {
+        'street light': 'lighting',
+        'dark': 'lighting',
+        'light': 'lighting',
+        'road surface': 'road',
+        'asphalt': 'road',
+        'path': 'road',
+        'tree': 'landscaping',
+        'plant': 'landscaping',
+        'dirt': 'landscaping',
+        'water': 'plumbing'
+                  }
+    result_dict = {
+        'classes': [],
+        'spam trigger': False,
+        'inappropriate image': False
+    }
+    result_dict = check_adult_score(analysis['adult']['adultScore'], result_dict)
+    result_dict['classes'] = define_class(tags, class_dict)
+    result_dict = check_for_spam(result_dict['classes'], result_dict)
+​
+    app_json = json.dumps(result_dict)
+    print(app_json)
+​
+​
+def check_adult_score(adult_score, result_dict):
+    if adult_score >= 0.1:
+        result_dict['inappropriate image'] = True
+    return result_dict
+​
+​
+def define_class(tags, class_dict):
+    classes = []
+    for tag in tags:
+        if tag in class_dict.keys():
+            classes.append(class_dict[tag])
+    return classes
+​
+​
+def check_for_spam(classes, result_dict):
+    if len(classes) == 0:
+        result_dict['spam trigger'] = True
+    return result_dict
+​
+​
+if __name__ == '__main__':
     main()
